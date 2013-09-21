e97caa3d0dda44d1eca79880690394867bffb946
thecatkin <thecatkin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-21 15:57:36 +0000

    Updated link to upcoming reservation demo page
    git-svn-id: https://plugins.svn.wordpress.org/redi-restaurant-reservation/trunk@776422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/basicpackage.php
+++ templates/basicpackage.php
@@ -5,9 +5,9 @@
 	<h2><?php _e('Basic package (paid version)', 'redi-restaurant-reservation'); ?></h2>
 		<br/>
 		<p class="description">
-View your upcoming reservations from your Mobile/Tablet PC and never miss your customer. This page should be open on a Tablet PC and so waitress can see all upcoming reservations for today. Page refreshes every 15 min and shows reservations that in past for 3 hours as well as upcoming reservations for next 24 hours. By clicking on reservation you will see reservation details.<br/>
+View your upcoming reservations from your Mobile/Tablet PC and never miss your customer. This page should be open on a Tablet PC so waitress can see all upcoming reservations for today. Page refreshes every 15 min and shows reservations that in past for 3 hours as well as all upcoming reservations for next 24 hours. By clicking on reservation item you will see reservation details.<br/>
 <br/>
-Demo version can be accessed by this link: <a href="http://goo.gl/DFSBXQ" target="_blank">Open demo version</a>
+Fully functional demo can be accessed by following link for 30 days: <a href="http://upcoming.reservationdiary.eu/Entry/<?php _e($options[REDI_APIKEY]) ?>" target="_blank">Open upcoming reservations</a>
 <br/>
 <br/>
 For paid packages please contact us by email: <a href="mailto:info@reservationdiary.eu">info@reservationdiary.eu</a>
