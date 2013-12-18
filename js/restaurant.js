68c6a839172e81cf3aadaa88d99cf6cc7d5a97f6
robby.roboter <robby.roboter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-18 10:48:17 +0000

    disable submit button to prevent multiple submits
    git-svn-id: https://plugins.svn.wordpress.org/redi-restaurant-reservation/branches/development@824607 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/restaurant.js
+++ js/restaurant.js
@@ -120,7 +120,9 @@ jQuery(function () {
 
         jQuery('#step3load').show();
         jQuery('#step3errors').hide('slow');
+        jQuery('#redi-restaurant-step3').attr('disabled', true);
         jQuery.post(AjaxUrl.ajaxurl, data, function (response) {
+            jQuery('#redi-restaurant-step3').attr('disabled', false);
             jQuery('#step3load').hide();
             if (response['Error']) {
                 jQuery('#step3errors').html(response['Error']).show('slow');
