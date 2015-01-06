26d271cb76cfc8a2bf8119ba39bb4fa12b8326c2
robby.roboter <robby.roboter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-06 19:29:34 +0000

    Reformat, class exists check
    git-svn-id: https://plugins.svn.wordpress.org/redi-restaurant-reservation/trunk@1061576 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- redi.php
+++ redi.php
@@ -5,76 +5,80 @@
  * @time: 20:03
  *
  */
-if (!defined('REDI_RESTAURANT_DEBUG'))
-	define('REDI_RESTAURANT_DEBUG', FALSE);
 
-if (!defined('USER'))
+if (!defined('REDI_RESTAURANT_DEBUG')) {
+	define( 'REDI_RESTAURANT_DEBUG', false );
+}
+if (!defined('USER')) {
 	define('USER', 'User.svc/');
-
-if (!defined('USERGET'))
+}
+if (!defined('USERGET')) {
 	define('USERGET', 'User.svc/get');
-
-if (!defined('PLACE')) // 'Place.svc/place' - for old version
+}
+if (!defined('PLACE')) { // 'Place.svc/place' - for old version
 	define('PLACE', 'Place.svc/');
-
-if (!defined('SERVICE'))
+}
+if (!defined('SERVICE')) {
 	define('SERVICE', 'Service.svc/');
-
-if (!defined('CATEGORY'))
+}
+if (!defined('CATEGORY')) {
 	define('CATEGORY', 'Category.svc/');
-
-if (!defined('RESERVATION'))
+}
+if (!defined('RESERVATION')) {
 	define ('RESERVATION', 'Reservation.svc/');
-
-if(!defined('EMAILCONTENT'))
+}
+if(!defined('EMAILCONTENT')) {
     define('EMAILCONTENT','emailcontent.svc/');
-
-if (!defined('POST'))
+}
+if (!defined('POST')) {
 	define('POST', 'POST');
-
-if (!defined('GET'))
+}
+if (!defined('GET')) {
 	define('GET', 'GET');
-
-if (!defined('PUT'))
+}
+if (!defined('PUT')) {
 	define('PUT', 'PUT');
-
-if (!defined('DELETE'))
+}
+if (!defined('DELETE')) {
 	define('DELETE', 'DELETE');
-
-if (!defined('REDI_SUCCESS'))
+}
+if (!defined('REDI_SUCCESS')) {
 	define('REDI_SUCCESS', 'SUCCESS');
-
-if (!defined('REDI_RESTAURANT_API'))
-{
+}
+if ( ! defined( 'REDI_RESTAURANT_API' ) ) {
 	define('REDI_RESTAURANT_API', 'http://api.reservationdiary.eu/service/');
 }
 
+if ( ! defined( 'CUSTOM_FIELDS' ) ) {
 define('CUSTOM_FIELDS', 6);
+}
 
-class Redi
-{
+if ( ! class_exists( 'ReDi' ) ) {
+	class Redi {
 	private $ApiKey;
 
-	public function Redi($ApiKey)
-	{
+		public function Redi( $ApiKey ) {
 		$this->ApiKey = $ApiKey;
 	}
 
     public function getEmailContent($reservationID, $type, $params){
-        return $this->request(REDI_RESTAURANT_API.EMAILCONTENT.$this->ApiKey.'/'.$reservationID.'/ClientReservation'.$type, GET, $this->strParams($params));
+			return $this->request( REDI_RESTAURANT_API . EMAILCONTENT . $this->ApiKey . '/' . $reservationID . '/ClientReservation' . $type,
+				GET, $this->strParams( $params ) );
     }
 
 	public function cancelReservationByClient( $params ) {
-		return $this->request( REDI_RESTAURANT_API . RESERVATION . $this->ApiKey . '/cancelByClient', DELETE, $this->strParams( $params ) );
+			return $this->request( REDI_RESTAURANT_API . RESERVATION . $this->ApiKey . '/cancelByClient', DELETE,
+				$this->strParams( $params ) );
 	}
 
 	public function cancelReservation( $params ) {
-		return $this->request( REDI_RESTAURANT_API . RESERVATION . $this->ApiKey . '/cancelByProvider', DELETE, $this->strParams( $params ) );
+			return $this->request( REDI_RESTAURANT_API . RESERVATION . $this->ApiKey . '/cancelByProvider', DELETE,
+				$this->strParams( $params ) );
 	}
 
-	public function createReservation($categoryID, $params)
-	{
-		return $this->request(REDI_RESTAURANT_API.RESERVATION.$this->ApiKey.'/'.$categoryID, POST, json_encode(self::unescape_array($params)));
+		public function createReservation( $categoryID, $params ) {
+			return $this->request( REDI_RESTAURANT_API . RESERVATION . $this->ApiKey . '/' . $categoryID, POST,
+				json_encode( self::unescape_array( $params ) ) );
 	}
 
 	/**
@@ -86,122 +90,111 @@ class Redi
 	 * Quantity
 	 * Alternatives
 	 * </pre>
+		 *
 	 * @return array
 	 */
-	public function query($categoryID, $params)
-	{
-		return $this->request(REDI_RESTAURANT_API.RESERVATION.$this->ApiKey.'/'.$categoryID.'/Person', GET, $this->strParams($params));
+		public function query( $categoryID, $params ) {
+			return $this->request( REDI_RESTAURANT_API . RESERVATION . $this->ApiKey . '/' . $categoryID . '/Person',
+				GET, $this->strParams( $params ) );
 	}
 
-	public function createCategory($placeID, $params)
-	{
-		return $this->request(REDI_RESTAURANT_API.CATEGORY.$this->ApiKey.'/'.$placeID, POST, json_encode(self::unescape_array($params)));
+		public function createCategory( $placeID, $params ) {
+			return $this->request( REDI_RESTAURANT_API . CATEGORY . $this->ApiKey . '/' . $placeID, POST,
+				json_encode( self::unescape_array( $params ) ) );
 	}
 
-	public function getServices($categoryID)
-	{
+		public function getServices( $categoryID ) {
 		return $this->request(REDI_RESTAURANT_API.SERVICE.$this->ApiKey.'/'.$categoryID.'/Person', GET);
 	}
 
-	public function deleteServices($ids)
-	{
-		return $this->request(REDI_RESTAURANT_API.SERVICE.$this->ApiKey.'?serviceID='.join(',', $ids), DELETE);
+		public function deleteServices( $ids ) {
+			return $this->request( REDI_RESTAURANT_API . SERVICE . $this->ApiKey . '?serviceID=' . join( ',', $ids ),
+				DELETE );
 	}
 
-	public function setServiceTime($categoryID, $timeSet)
-	{
+		public function setServiceTime( $categoryID, $timeSet ) {
 		return $this->request(REDI_RESTAURANT_API.CATEGORY.$this->ApiKey.'/'.$categoryID.'/time',
 			PUT,
 			json_encode(self::unescape_array(array ('timeSet' => $timeSet))));
 	}
 
-	public function getServiceTime($categoryID)
-	{
+		public function getServiceTime( $categoryID ) {
 		return $this->request(REDI_RESTAURANT_API.CATEGORY.$this->ApiKey.'/'.$categoryID.'/time', GET);
 	}
 
-	public function createService($categoryID, $params)
-	{
-		return $this->request(REDI_RESTAURANT_API.SERVICE.$this->ApiKey.'/'.$categoryID, POST, json_encode(self::unescape_array($params)));
+		public function createService( $categoryID, $params ) {
+			return $this->request( REDI_RESTAURANT_API . SERVICE . $this->ApiKey . '/' . $categoryID, POST,
+				json_encode( self::unescape_array( $params ) ) );
 	}
 
-	public function userGetError()
-	{
+		public function userGetError() {
 		return $this->request(REDI_RESTAURANT_API.USERGET);
 	}
 
-	public function createUser($params)
-	{
+		public function createUser( $params ) {
 		return $this->request(REDI_RESTAURANT_API.USER, POST, json_encode(self::unescape_array($params)));
 	}
 
-	public function setPlace($placeID, $params)
-	{
-		return $this->request(REDI_RESTAURANT_API.PLACE.$this->ApiKey.'/'.$placeID, PUT, json_encode(self::unescape_array($params)));
+		public function setPlace( $placeID, $params ) {
+			return $this->request( REDI_RESTAURANT_API . PLACE . $this->ApiKey . '/' . $placeID, PUT,
+				json_encode( self::unescape_array( $params ) ) );
 	}
 
-	public function createPlace($params)
-	{
-		return $this->request(REDI_RESTAURANT_API.PLACE.$this->ApiKey, POST, json_encode(self::unescape_array($params)));
-	}
-	public function shiftsStartTime($categoryID, $params)
-	{
-		return $this->request(REDI_RESTAURANT_API.CATEGORY.$this->ApiKey.'/'.$categoryID.'/shiftsStartTime', GET, $this->strParams($params));
+		public function createPlace( $params ) {
+			return $this->request( REDI_RESTAURANT_API . PLACE . $this->ApiKey, POST,
+				json_encode( self::unescape_array( $params ) ) );
 	}
 
-	public function availabilityByDay( $categoryID, $params )
-	{
-		return $this->request(REDI_RESTAURANT_API.RESERVATION.$this->ApiKey.'/'.$categoryID.'/availabilityByDay/Person', GET, $this->strParams($params));
+		public function shiftsStartTime( $categoryID, $params ) {
+			return $this->request( REDI_RESTAURANT_API . CATEGORY . $this->ApiKey . '/' . $categoryID . '/shiftsStartTime',
+				GET, $this->strParams( $params ) );
 	}
 
-	public function availabilityByShifts($categoryID, $params)
-	{
-        return $this->request(REDI_RESTAURANT_API.RESERVATION.$this->ApiKey.'/'.$categoryID.'/availabilityByShifts/Person', GET, $this->strParams($params));
+		public function availabilityByDay( $categoryID, $params ) {
+			return $this->request( REDI_RESTAURANT_API . RESERVATION . $this->ApiKey . '/' . $categoryID . '/availabilityByDay/Person',
+				GET, $this->strParams( $params ) );
 	}
 
-	public function getPlace($placeID)
-	{
+		public function availabilityByShifts( $categoryID, $params ) {
+			return $this->request( REDI_RESTAURANT_API . RESERVATION . $this->ApiKey . '/' . $categoryID . '/availabilityByShifts/Person',
+				GET, $this->strParams( $params ) );
+	}
+
+		public function getPlace( $placeID ) {
 		return $this->request(REDI_RESTAURANT_API.PLACE.$this->ApiKey.'/'.$placeID, GET);
 	}
     
-    public function getPlaceCategories($placeID)
-	{
+		public function getPlaceCategories( $placeID ) {
 		return $this->request(REDI_RESTAURANT_API.PLACE.$this->ApiKey.'/'.$placeID.'/categories', GET);
 	}
 
-    public function getPlaces()
-	{
+		public function getPlaces() {
 		return $this->request(REDI_RESTAURANT_API.PLACE.$this->ApiKey, GET);
 	}
     
-	public function setApiKey($ApiKey)
-	{
+		public function setApiKey( $ApiKey ) {
 		$this->ApiKey = $ApiKey;
 	}
 
-	public function strParams($params)
-	{
+		public function strParams( $params ) {
 		$url_param = '';
 		$first = 0;
 
-		if (is_array($params))
-			foreach ($params as $param_name => $param_value)
+			if ( is_array( $params ) ) {
+				foreach ( $params as $param_name => $param_value ) {
 				$url_param .= (($first++ == 0) ? '?' : '&').$param_name.'='.$param_value;
+				}
+			}
 
 		return $url_param;
 	}
 
-	private static function unescape_array($array)
-	{
+		private static function unescape_array( $array ) {
 		$unescaped_array = array();
-		foreach($array as $key => $val)
-		{
-			if(is_array($val))
-			{
+			foreach ( $array as $key => $val ) {
+				if ( is_array( $val ) ) {
 				$unescaped_array[$key] = self::unescape_array($val);
-			}
-			else
-			{
+				} else {
 				$unescaped_array[$key] = stripslashes($val);
 			}
 		}
@@ -223,13 +216,17 @@ class Redi
 		if ( is_wp_error( $output ) ) {
 			return array(
 				'request_time' => round( microtime( true ) - $curTime, 3 ) * 1000,
-				'Error'    => __( 'Online reservation service is not available at this time. Try again later or contact us directly.', 'redi-restaurant-reservation' ),
+					'Error'        => __( 'Online reservation service is not available at this time. Try again later or contact us directly.',
+						'redi-restaurant-reservation' ),
 				'Wp-Error' => $output->errors
 			);
 		}
 
 		if ( $output['response']['code'] != 200 && $output['response']['code'] != 400 ) {
-			return array( 'Error' => __( 'Online reservation service is not available at this time. Try again later or contact us directly.', 'redi-restaurant-reservation' ) );
+				return array(
+					'Error' => __( 'Online reservation service is not available at this time. Try again later or contact us directly.',
+						'redi-restaurant-reservation' )
+				);
 		}
 		$output = $output['body'];
 
@@ -240,7 +237,8 @@ class Redi
 			(
 				'method' => $method,
 				'params' => self::p( $params_string, true ),
-				'url'    => self::p( $url . ( ( $method == GET || $method == DELETE ) ? $params_string : '' ), true ),
+					'url'    => self::p( $url . ( ( $method == GET || $method == DELETE ) ? $params_string : '' ),
+						true ),
 			);
 		}
 
@@ -257,7 +255,8 @@ class Redi
 				return;
 			}
 			$result = highlight_string( "<?php\n" . print_r( $object, true ), true );
-			echo '<pre style="text-align: left;">'.preg_replace('/&lt;\\?php<br \\/>/', '', $result, 1).'</pre><br />';
+				echo '<pre style="text-align: left;">' . preg_replace( '/&lt;\\?php<br \\/>/', '', $result,
+						1 ) . '</pre><br />';
 		}
 
 	}
@@ -266,5 +265,6 @@ class Redi
 		if ( REDI_RESTAURANT_DEBUG ) {
 			return var_export( $object, true );
 	}
+}
 }
 }
